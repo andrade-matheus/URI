f108289e091e4871d15c525c67ae485868a88fd9
Matheus Andrade <mat.tonelliandrade@gmail.com>
2019-07-24 22:52:11 -0400

    2876 fazendo

--- Python/2876.py
+++ Python/2876.py
@@ -4,9 +4,13 @@ cont = 0
 cont += (M*(N-1)) + (N*(M-1))
 print('Resultado  =', (M*(N-1)) + (N*(M-1)))
 print('Contagem  =', cont)
-cont += (M-1) * (N*(N-1))
-print('Resultado  =', (M-1) * (N*(N-1)))
-print('Contagem  =', cont)
-cont += (N-1) * ((M*(M-3))+2)
-print('Resultado  =', (N-1) * ((M*(M-3))+2))
-print('Contagem  =', cont)
+
+if (R > 1):
+    cont += (M-1) * (N*(N-1))
+    print('Resultado  =', (M-1) * (N*(N-1)))
+    print('Contagem  =', cont)
+
+if(M > 2):
+    cont += (N-1) * ((M*(M-3))+2)
+    print('Resultado  =', (N-1) * ((M*(M-3))+2))
+    print('Contagem  =', cont)
