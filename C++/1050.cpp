89f737d45c942fc1bac1570635ca1dee6325aa9a
Matheus Andrade <mat.tonelliandrade@gmail.com>
2020-03-18 10:19:05 -0400

    Resolvido 1050

---
+++ C++/1050.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main() {
+  int ddd;
+
+  cin >> ddd;
+
+  if (ddd == 61) {
+    cout << "Brasilia";
+  }
+  else if (ddd == 71) {
+    cout << "Salvador";
+  }else if (ddd == 11) {
+    cout << "Sao Paulo";
+  }else if (ddd == 21) {
+    cout << "Rio de Janeiro";
+  }else if (ddd == 32) {
+    cout << "Juiz de Fora";
+  }else if (ddd == 19) {
+    cout << "Campinas";
+  }else if (ddd == 27) {
+    cout << "Vitoria";
+  }else if (ddd == 31) {
+    cout << "Belo Horizonte";
+  }else{
+    cout << "DDD nao cadastrado";
+  }
+  
+  cout << endl;
+
+  return 0;
+}
