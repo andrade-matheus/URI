2352b8f714b588646eabb39592b6dddb6c662685
Matheus Andrade <mat.tonelliandrade@gmail.com>
2020-03-25 10:00:52 -0400

    Resolvido 2755

---
+++ C++/2755.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  std::cout << "\"Ro'b'er\tto\\/\"" << '\n';
+  std::cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << '\n';
+  std::cout << "(^_-) (-_-) (-_^)" << '\n';
+  std::cout << "(\"_\") (\'.\')" << '\n';
+
+  return 0;
+}
