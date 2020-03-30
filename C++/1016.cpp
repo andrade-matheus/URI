aad7415eab9f66b5347fc5c6a1f034c0a338323f
Matheus Andrade <mat.tonelliandrade@gmail.com>
2020-03-30 12:09:49 -0400

    Resolvido 1016

---
+++ C++/1016.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int distancia;
+
+  std::cin >> distancia;
+
+  std::cout << distancia * 2 << " minutos" << '\n';
+
+  return 0;
+}
